c1336a38513bef68c135c036be36f008b6807a03
RevSkyMC <160483771+ytRevalMC@users.noreply.github.com>
2026-05-26 13:16:42 +0700

    Update RevSkyMorph.java

--- src/main/java/me/revsky/morph/RevSkyMorph.java
+++ src/main/java/me/revsky/morph/RevSkyMorph.java
@@ -1,1 +1,36 @@
+me.revsky.morph;
 
+import me.revsky.morph.commands.DisguiseCommand;
+import me.revskypackage.morph.commands.UndisguiseCommand;
+import me.revsky.morph.listeners.JoinListener;
+import me.revsky.morph.managers.MorphManager;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class RevSkyMorph extends JavaPlugin {
+
+    private static RevSkyMorph instance;
+    private MorphManager morphManager;
+
+    @Override
+    public void onEnable() {
+        instance = this;
+
+        morphManager = new MorphManager();
+
+        getCommand("disguise").setExecutor(new DisguiseCommand(morphManager));
+        getCommand("undisguise").setExecutor(new UndisguiseCommand(morphManager));
+
+        getServer().getPluginManager().registerEvents(new JoinListener(), this);
+
+        getLogger().info("RevSky Morph Enabled!");
+    }
+
+    @Override
+    public void onDisable() {
+        getLogger().info("RevSky Morph Disabled!");
+    }
+
+    public static RevSkyMorph getInstance() {
+        return instance;
+    }
+}
